bca071dfeba0ab2224b0cf2a0c49561b04c8df5d
Arthur Magomedov <devmagomedov@gmail.com>
2022-11-28 17:43:41 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008]

--- src/main/java/ru/job4j/map/SimpleMap.java
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -1,6 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Iterator;
+import java.util.*;
 
 public class SimpleMap<K, V> implements Map<K, V> {
 
@@ -16,34 +16,95 @@ public class SimpleMap<K, V> implements Map<K, V> {
 
     @Override
     public boolean put(K key, V value) {
-        return false;
+        boolean result = false;
+        expand();
+
+        int hashCodeKey = hash(Objects.hashCode(key));
+        int index = indexFor(hashCodeKey);
+
+        if (table[index] == null) {
+            table[index] = new MapEntry<>(key, value);
+            result = true;
+            count++;
+            modCount++;
+        }
+        return result;
     }
 
     private int hash(int hashCode) {
-        return 0;
+        return hashCode ^ (hashCode >>> 16);
     }
 
     private int indexFor(int hash) {
-        return 0;
+        return hash % (table.length - 1);
     }
 
     private void expand() {
-
+        if (count == (capacity * LOAD_FACTOR)) {
+            capacity *= 2;
+            table = Arrays.copyOf(table, capacity);
+        }
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V result = null;
+
+        int hashCodeKey = hash(Objects.hashCode(key));
+        int index = indexFor(hashCodeKey);
+
+        if (table[index] != null) {
+            int hashCodeKey2 = hash(Objects.hashCode(table[index].key));
+            if (hashCodeKey == hashCodeKey2) {
+                if (Objects.equals(key, table[index].key)) {
+                    result = table[index].value;
+                }
+            }
+        }
+        return result;
     }
 
     @Override
     public boolean remove(K key) {
-        return false;
+        boolean result = false;
+        if (get(key) != null) {
+            int hashCodeKey = hash(Objects.hashCode(key));
+            int index = indexFor(hashCodeKey);
+            table[index] = null;
+            result = true;
+            count--;
+            modCount++;
+        }
+        return result;
     }
 
     @Override
     public Iterator<K> iterator() {
-        return null;
+        return new Iterator<K>() {
+            final int expectedModCount = modCount;
+            private int point = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                while (point < table.length && table[point] == null) {
+                    point++;
+                }
+                return point < table.length;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                K key = table[point].key;
+                point++;
+                return key;
+            }
+        };
     }
 
     private static class MapEntry<K, V> {
@@ -55,7 +116,5 @@ public class SimpleMap<K, V> implements Map<K, V> {
             this.key = key;
             this.value = value;
         }
-
     }
-
 }
