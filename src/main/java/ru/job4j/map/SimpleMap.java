2e28da196474469999ddc8f8dce80372a20b0164
Arthur Magomedov <devmagomedov@gmail.com>
2022-11-30 11:44:36 +0300

    8. Реализовать собственную структуру данных - HashMap [#1008 #365362]

--- src/main/java/ru/job4j/map/SimpleMap.java
+++ src/main/java/ru/job4j/map/SimpleMap.java
@@ -33,8 +33,7 @@ public class SimpleMap<K, V> implements Map<K, V> {
     }
 
     private int indexFor(int hash) {
-        //return hash % (table.length - 1);
-        return hash & capacity - 1;
+        return hash & (capacity - 1);
     }
 
     private void expand() {
@@ -43,7 +42,7 @@ public class SimpleMap<K, V> implements Map<K, V> {
             capacity *= 2;
             MapEntry<K, V>[] tempTable = new MapEntry[capacity];
             for (int i = 0; i < limit; i++) {
-                if (table[i].value != null) {
+                if (table[i] != null) {
                     int index = indexByKey(table[i].key);
                     tempTable[index] = new MapEntry<>(table[i].key, table[i].value);
                 }
