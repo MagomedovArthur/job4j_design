754603d5a0038fcf008dd26b23bc2dfb9b65627a
Arthur Magomedov <devmagomedov@gmail.com>
2022-12-09 13:38:52 +0300

    0.1. FileOutputStream. [#252488 #368910]

--- src/main/java/ru/job4j/io/MultiplicationTable.java
+++ src/main/java/ru/job4j/io/MultiplicationTable.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class MultiplicationTable {
@@ -14,7 +15,7 @@ public class MultiplicationTable {
                 }
                 out.write(System.lineSeparator().getBytes());
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
