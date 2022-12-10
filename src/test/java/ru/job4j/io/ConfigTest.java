f769956ac67d04defa25d65193b1844dcd6b42f2
Arthur Magomedov <devmagomedov@gmail.com>
2022-12-10 17:29:36 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,69 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.url")).isEqualTo("jdbc:postgresql");
+    }
+
+    @Test
+    void whenNotContainsKey() {
+        IllegalArgumentException exception = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    String path = "./data/pair_not_contains_key.properties";
+                    Config config = new Config(path);
+                    config.load();
+                }
+        );
+        assertThat(exception.getMessage()).isEqualTo("File contains errors!");
+    }
+
+    @Test
+    void whenNotContainsEqualsSign() {
+        IllegalArgumentException exception = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    String path = "./data/pair_does_not contain_equals_sign.properties";
+                    Config config = new Config(path);
+                    config.load();
+                }
+        );
+        assertThat(exception.getMessage()).isEqualTo("File contains errors!");
+    }
+
+    @Test
+    void whenNotContainsKeyAndValue() {
+        IllegalArgumentException exception = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    String path = "./data/pair_not_contains_key_and_value.properties";
+                    Config config = new Config(path);
+                    config.load();
+                }
+        );
+        assertThat(exception.getMessage()).isEqualTo("File contains errors!");
+    }
+
+    @Test
+    void whenContainsComments() {
+        IllegalArgumentException exception = Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    String path = "./data/file_contains_comments.properties";
+                    Config config = new Config(path);
+                    config.load();
+                }
+        );
+        assertThat(exception.getMessage()).isEqualTo("File contains errors!");
+    }
+}
