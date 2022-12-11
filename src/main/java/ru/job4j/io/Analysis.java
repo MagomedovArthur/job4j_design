3a26b99f16a43f7c47353d4a0cdf7a4717c9e49b
Arthur Magomedov <devmagomedov@gmail.com>
2022-12-11 16:13:56 +0300

    2. Анализ доступности сервера. [#859 #369961]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,17 +6,19 @@ public class Analysis {
 
     public void unavailable(String source, String target) {
         var flag = true;
-        try (PrintWriter writer = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            BufferedReader reader = new BufferedReader(new FileReader(source));
+        try (PrintWriter writer = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)));
+             BufferedReader reader = new BufferedReader(new FileReader(source))) {
             for (String line = reader.readLine(); line != null; line = reader.readLine()) {
                 if (flag && (line.contains("400") || line.contains("500"))) {
                     flag = false;
                     String[] array = line.split(" ", 2);
-                    writer.print(array[1] + ";");
+                    writer.print(array[1]);
+                    writer.print(";");
                 } else if (!flag && (line.contains("200") || line.contains("300"))) {
                     flag = true;
                     String[] array = line.split(" ", 2);
-                    writer.print(array[1] + ";");
+                    writer.print(array[1]);
+                    writer.print(";");
                     writer.print(System.lineSeparator());
                 }
             }
