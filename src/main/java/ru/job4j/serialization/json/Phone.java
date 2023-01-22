23ddf0e8bba64b72002107f4dab5a714c6b8b629
Arthur Magomedov <devmagomedov@gmail.com>
2023-01-23 02:36:22 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/json/Phone.java
@@ -0,0 +1,51 @@
+package ru.job4j.serialization.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+
+public class Phone {
+
+    private final boolean activated;
+    private final int yearOfManufacture;
+    private final String serialNumber;
+    private final PhoneOwner phoneOwner;
+    private final String[] installedApplications;
+
+    public Phone(boolean activated, int yearOfManufacture,
+                 String serialNumber, PhoneOwner phoneOwner,
+                 String[] installedApplications) {
+        this.activated = activated;
+        this.yearOfManufacture = yearOfManufacture;
+        this.serialNumber = serialNumber;
+        this.phoneOwner = phoneOwner;
+        this.installedApplications = installedApplications;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{"
+                + "activated=" + activated
+                + ", yearOfManufacture=" + yearOfManufacture
+                + ", serialNumber='" + serialNumber + '\''
+                + ", phoneOwner=" + phoneOwner
+                + ", installedApplications=" + Arrays.toString(installedApplications)
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        PhoneOwner phoneOwner = new PhoneOwner("Artur", "Magomedov", 22);
+        Phone iphone13Pro = new Phone(true, 2022, "9595NMY005",
+                                       phoneOwner, new String[]{"Safari", "Fitness", "Chrome", "Photo"});
+
+        /* Преобразовние объекта iphone13Pro в json-строку. */
+        Gson gson = new GsonBuilder().create();
+        final String phoneJSON = gson.toJson(iphone13Pro);
+        System.out.println(gson.toJson(iphone13Pro));
+
+        /* Извлечение объекта iphone13Pro из json-строки. */
+        Phone phone = gson.fromJson(phoneJSON, Phone.class);
+        System.out.println(phone);
+    }
+}
---
+++ src/main/java/ru/job4j/serialization/json/PhoneOwner.java
@@ -0,0 +1,23 @@
+package ru.job4j.serialization.json;
+
+public class PhoneOwner {
+
+    private final String name;
+    private final String lastName;
+    private final int age;
+
+    public PhoneOwner(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneOwner{"
+                + "name='" + name + '\''
+                + ", lastName='" + lastName + '\''
+                + ", age=" + age
+                + '}';
+    }
+}
